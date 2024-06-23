90cae3b4803139e1d4ca3c5faa01ea05677e2678
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-23 12:05:20 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Contact from "./Contact";
 import Experience from "./Experience";
-import Hero from "./hero";
+import Hero from "./Hero";
 import Portfolio from "./Portfolio";
 import ModelCanvas from "./of_planes_and_satellites";
 
