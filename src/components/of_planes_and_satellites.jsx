77b5ec600072d36e957647cd1f75801958178677
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-23 16:31:37 -0400

    Update of_planes_and_satellites.jsx

--- src/components/of_planes_and_satellites.jsx
+++ src/components/of_planes_and_satellites.jsx
@@ -28,7 +28,7 @@ const ModelCanvas = ({ scrollContainer }) => {
     console.log("ModelCanvas loaded");
     const [rotationX, setRotationX] = useState(0);
     const [rotationY, setRotationY] = useState(0);
-    const [scale, setScale] = useState([1, 1, 1]);
+    const [scale, setScale] = useState([100, 100, 100]);
     const [position, setPosition] = useState([0, 0, 0]);
 
     useEffect(() => {
