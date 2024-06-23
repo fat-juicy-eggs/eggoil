9b5c0bb4f282cca9a970f4c707ba7b715d8e347d
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-23 15:12:29 -0400

    Update of_planes_and_satellites.jsx

--- src/components/of_planes_and_satellites.jsx
+++ src/components/of_planes_and_satellites.jsx
@@ -4,15 +4,18 @@ import { Suspense, useEffect, useRef, useState } from "react";
 import coolModel from "../assets/of_planes_and_satellites.glb";
 import CanvasLoader from "./Loader";
 
-const theModel = ({ scale, position }) => {
+const TheModel = ({ scale, position }) => {
     const modelRef = useRef();
     const { scene, animations } = useGLTF(coolModel);
     const  { actions } = useAnimations(animations, modelRef);
 
     useEffect(() => {
-        actions["Idle"].play();
+        if (actions && actions["Idle"]) {
+            actions["Idle"].play();
+        } else {
+            console.error("Idle action not found in animations");
+        }
     }, [actions]);
-
     return (
         <mesh ref={modelRef} position={position} scale={scale} rotation={[-0.3, 0, 3]}>
             <primitive object={scene} />
@@ -65,14 +68,14 @@ const ModelCanvas = ({ scrollContainer }) => {
 
     return (
         <Canvas className={'w-full h-screen bg-transparent z-10'} camera={{ near: 0.1, far: 1000 }}>
-            <Suspense>
+            <Suspense fallback={<CanvasLoader />}>
                 <directionalLight position={[1, 1, 1]} intensity={2} />
                 <ambientLight intensity={0.5} />
                 <pointLight position={[10, 5, 10]} intensity={2} />
                 <spotLight position={[0, 50, 10]} angle={0.15} penumbra={1} intensity={2} />
                 <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
                 
-                <theModel rotationX={rotationX} rotationY={rotationY} scale={scale} position={position} />
+                <TheModel rotationX={rotationX} rotationY={rotationY} scale={scale} position={position} />
             </Suspense>
         </Canvas>
     );
