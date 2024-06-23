125e1e77520e165dfc6888d6ffcb2adbc4048861
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-23 14:19:46 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Experience from "./Experience";
 import Hero from "./Hero";
 import Portfolio from "./Portfolio";
 import ModelCanvas from "./of_planes_and_satellites";
+import Navbar from "./Navbar";
 
 export {
-    Experience, Hero, Portfolio, ModelCanvas
+    Experience, Hero, Portfolio, ModelCanvas, Navbar
 };
