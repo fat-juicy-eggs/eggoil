b00d6aded163e9f0eef4da57e3684177e18ddfdd
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-25 21:18:17 -0400

    Update of_planes_and_satellites.jsx

--- src/components/of_planes_and_satellites.jsx
+++ src/components/of_planes_and_satellites.jsx
@@ -18,7 +18,7 @@ const TheModel = ({ scale, position }) => {
     }, [actions]);
 
     return (
-        <mesh ref={modelRef} position={position} scale={scale} rotation={[0, 0.8, 0.1]}>
+        <mesh ref={modelRef} position={position} scale={scale} rotation={[0, 0, 0]}>
             <primitive object={scene} />
         </mesh>
     );
@@ -28,7 +28,7 @@ const ModelCanvas = ({ scrollContainer }) => {
     console.log("ModelCanvas loaded");
     const [rotationX, setRotationX] = useState(0);
     const [rotationY, setRotationY] = useState(0);
-    const [scale, setScale] = useState([2, 2, 2]);
+    const [scale, setScale] = useState([0.33, 0.33, 0.33]);
     const [position, setPosition] = useState([0, -3, 0]);
 
     useEffect(() => {
