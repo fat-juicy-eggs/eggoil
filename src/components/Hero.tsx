25f1e02bf655229626812532caac3c12df9ca2a5
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-25 22:17:47 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const Hero = ({ scrollContainer }) => {
     return (
         <section id="hero" className="parallax">
-            <div className='absolute inset-x-0 bottom-0 w-full mx-auto flex flex-col items-start z-10'>
+            <div className='absolute right-0 bottom-0 w-full mx-auto flex flex-col items-start z-10'>
                 <div className='flex-1 mb-0'>
                     <h2 className='font-medium text-white text-[40px]'>
                         Scroll down.
