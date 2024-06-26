a95f0cd1489b2ba3ab565006add153e5029a63ad
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:40:17 -0400

    Update Bio.tsx

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -19,7 +19,7 @@ const Bio = () => {
                 &nbsp;
             </span>
             <div className="section">
-                <div className='w-full h-fit opacity-10 z-40'>
+                <div className='w-full h-fit z-40'>
                   <div className='flex flex-col w-1/2 text-center'>
                     <h2 className='text-white font-bold text-[40px]'>
                       Daniel Yi
