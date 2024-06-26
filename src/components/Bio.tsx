67f980ef5666b0714ca4bba5a1f5d155c2613964
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 13:56:33 -0400

    Create Bio.tsx

---
+++ src/components/Bio.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SectionWrapper from '../hoc/SectionWrapper';
+
+const Bio = () => {
+    return (
+        <div className="section">
+            <div className='w-full h-fit opacity-10 z-40'>
+              <div className='flex flex-col w-1/2 text-center'>
+                <h2 className='text-white font-bold text-[40px]'>
+                  Daniel Yi
+                </h2>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default SectionWrapper(Bio, 'bio');
