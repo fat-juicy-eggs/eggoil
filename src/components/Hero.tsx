f30f171674b7c1334c73c98b2828a56621b51ed3
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:46:17 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,6 @@ const Hero = ({ scrollContainer }) => {
             </div>
             <ModelCanvas scrollContainer={scrollContainer} />
         </section>
-        <br /><br /><br /><br />
     );
 };
 
