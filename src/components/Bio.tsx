f2d5f513fcd5010f6ebdf55d555e4af2cdb2214a
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:17:02 -0400

    Update Bio.tsx

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { SectionWrapper } from '../hoc';
+import { motion } from 'framer-motion';
+import { staggerContainer } from '../utils/motion';
 
 const Bio = () => {
     return (
-        <div className="section">
-            <div className='w-full h-fit opacity-10 z-40'>
-              <div className='flex flex-col w-1/2 text-center'>
-                <h2 className='text-white font-bold text-[40px]'>
-                  Daniel Yi
-                </h2>
-              </div>
+        <motion.section variants={staggerContainer()} initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
+            <div className="section">
+                <div className='w-full h-fit opacity-10 z-40'>
+                  <div className='flex flex-col w-1/2 text-center'>
+                    <h2 className='text-white font-bold text-[40px]'>
+                      Daniel Yi
+                    </h2>
+                  </div>
+                </div>
             </div>
-        </div>
+        </motion.section>
     );
 };
 
-export default SectionWrapper(Bio, 'bio');
+export default Bio;
