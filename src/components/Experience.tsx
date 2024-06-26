de51f81382f2708b8d1d2a23fd5e8fe43051da9c
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:06:25 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import SectionWrapper from '../hoc/SectionWrapper';
+import emailjs from "@emailjs/browser";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
 
-const Experience = () => {
-    return (
-        <div className="section">
-            <h1>Experience</h1>
-            <p>My Experience</p>
-        </div>
-    );
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+
+const Contact = () => {
+  return (
+    <div
+      className="md:m-12 md:px-48 flex flex-col sm:flex-row gap-10 overflow-hidden"
+    >
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={{
+          hidden: {
+            opacity: 0,
+            y: 100,
+          },
+          show: {
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: "tween",
+              duration: 1,
+              delay: 0.2,
+            },
+          },
+        }}
+        className='flex-[0.8] md:pb-40 mx-4 sm:mx-auto'
+      >
+        <h3 className={styles.sectionText}>Contact</h3>
+      </motion.div>
+    </div>
+  );
 };
 
-export default SectionWrapper(Experience, 'experience');
+export default SectionWrapper(Contact, "contact");
