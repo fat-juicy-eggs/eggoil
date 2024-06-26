ea2fa78da0957802402bbd122aa8ea78add85107
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:36:00 -0400

    Update Bio.tsx

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { SectionWrapper } from '../hoc';
+import { motion } from 'framer-motion';
 
 const Bio = () => {
+    const variants = {
+        hidden: {},
+        show: {
+            transition: {
+            staggerChildren: staggerChildren,
+            delayChildren: delayChildren || 0,
+        }}
+    }
     return (
-        <section className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
+        <motion.section variants={variants} initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
             <span className='hash-span' id='bio'>
                 &nbsp;
             </span>
@@ -15,7 +25,7 @@ const Bio = () => {
                   </div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
