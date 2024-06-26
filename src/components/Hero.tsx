a212947b063d15ff0185c11ac525b07560958c21
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 16:13:33 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,8 @@ const Hero = ({ scrollContainer }) => {
     return (
         <section id="hero" className="parallax">
             <div className='absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10'>
-                <div className="flex-1 lg:mb-0">
-                  <h4 className='font-medium text-white text-[15px] xs:text-[18px] sm:text-[25px] md:text-[30px] lg:text-[37px] 2xl:text-[67px] leading-[41px] 2xl:leading-[60px]'>
+                <div className="flex-1 lg:mb-0 align-bottom">
+                  <h4 className='font-medium text-white text-[15px] xs:text-[18px] sm:text-[25px] md:text-[30px] lg:text-[37px] 2xl:text-[67px] leading-[41px] 2xl:leading-[60px] text-align:left'>
                     Scroll down.
                   </h4>
                 </div>
