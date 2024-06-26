0897e2f48e2ba004de229e01e870a1ff14f956c5
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-26 14:00:56 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import ModelCanvas from "./of_planes_and_satellites";
 import Navbar from "./Navbar";
 import CanvasLoader from "./Loader";
 import AnimatedText from "./AnimText";
-
+import Bio from "./Bio";
 export {
-    CanvasLoader, Experience, Hero, Portfolio, ModelCanvas, Navbar, AnimatedText
+    CanvasLoader, Experience, Hero, Portfolio, ModelCanvas, Navbar, AnimatedText, Bio
 };
