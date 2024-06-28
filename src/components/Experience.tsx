3069fbb5eca843c67681c5a1d13592d0de3852db
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-28 14:58:08 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -17,6 +17,8 @@ const Experience = () => {
     <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
         <span className='hash-span' id='experience'>
             &nbsp;
+            &nbsp;
+            &nbsp;
         </span>
         <div className='text-center md:text-left md:px-20 lg:px-40'>
           <motion.div variants={textVariant()}>
