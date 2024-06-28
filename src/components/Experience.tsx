5fb5e3e395587d969ce0027c6c64e452f95cc176
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-28 11:34:35 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,7 @@
 import { motion, useAnimation } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import { useInView } from 'react-intersection-observer';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Experience = () => {
   const controls = useAnimation();
@@ -17,30 +18,9 @@ const Experience = () => {
         <span className='hash-span' id='experience'>
             &nbsp;
         </span>
-        <div
-          className="md:m-12 md:px-48 flex flex-col sm:flex-row gap-10 overflow-hidden"
-        >
-          <motion.div
-            initial="hidden"
-            animate={controls}
-            variants={{
-              hidden: {
-                opacity: 0,
-                y: 100,
-              },
-              show: {
-                opacity: 1,
-                y: 0,
-                transition: {
-                  type: "tween",
-                  duration: 1,
-                  delay: 0.2,
-                },
-              },
-            }}
-            className='flex-[0.8] md:pb-40 mx-4 sm:mx-auto'
-          >
-            <h3>Contact</h3>
+        <div className='text-center md:text-left md:px-20 lg:px-40'>
+          <motion.div variants={textVariant()}>
+            <h2 className='text-white font-bold md:text-[80px] sm:text-[50px] text-[40px]'>Research: NIST SHIP</h2>
           </motion.div>
         </div>
     </motion.section>
