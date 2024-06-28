96e194d5390f6fc8417ad319ddaad72a5e77ced1
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-28 15:13:08 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -17,12 +17,18 @@ const Experience = () => {
     <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
         <span className='hash-span' id='experience'>
             &nbsp;
-            &nbsp;
-            &nbsp;
         </span>
         <div className='text-center md:text-left md:px-20 lg:px-40'>
           <motion.div variants={textVariant()}>
             <h2 className='text-white font-bold md:text-[60px] sm:text-[37px] text-[30px]'>Research: NIST SHIP</h2>
+          </motion.div>
+        </div>
+        <div className='w-full mt-[-2px] flex gap-5'>
+          <motion.div ref={ref} animate={controls} initial='hidden' variants={fadeIn("up", "spring", 0, 0.75)}>
+            <h4 className='text-white md:text-[30px] sm:text-[18px] text-[15px]'>This is the title of the paper</h4>
+            <div className='relative w-full md:w-3/5'>
+              <embed src="https://www.buds.com.ua/images/Lorem_ipsum.pdf" />
+            </div>
           </motion.div>
         </div>
     </motion.section>
