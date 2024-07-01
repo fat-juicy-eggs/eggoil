1afd164bd93b68cea57baf9c1defc0999b458140
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-06-30 23:45:27 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -14,7 +14,7 @@ const Experience = () => {
       }
   }, [controls, inView]);
   return (
-    <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0 h-full'>
+    <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0 h-[1vh]'>
         <span className='hash-span' id='experience'>
             &nbsp;
         </span>
@@ -23,7 +23,7 @@ const Experience = () => {
             <h2 className='text-white font-bold md:text-[60px] sm:text-[37px] text-[30px]'>Research: NIST SHIP</h2>
           </motion.div>
         </div>
-        <div className='w-full mt-[-2px] flex gap-5 mx-2'>
+        <div className='w-full mt-[-2px] flex gap-5 mx-2 h-full'>
           <motion.div ref={ref} animate={controls} initial='hidden' variants={fadeIn("up", "spring", 0, 0.75)}>
             <h4 className='text-white md:text-[30px] sm:text-[18px] text-[15px]'>This is the title of the paper</h4>
             <div className='relative w-full mx-[5px] h-full'>
