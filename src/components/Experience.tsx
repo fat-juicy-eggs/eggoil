ba965a9735ddc1781f68942a22c50ddcd74aa7ec
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-02 14:51:49 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,6 +2,7 @@ import { motion, useAnimation } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import { useInView } from 'react-intersection-observer';
 import { fadeIn, textVariant } from '../utils/motion';
+import ImageGallery from "react-image-gallery";
 
 const Experience = () => {
   const controls = useAnimation();
@@ -13,6 +14,14 @@ const Experience = () => {
           controls.start("show");
       }
   }, [controls, inView]);
+  const images = [
+    {
+      original: "https://images.unsplash.com/photo-1718358810029-5394de14abe7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      original: "https://images.unsplash.com/photo-1719742379974-dcf3a4c478b8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
   return (
     <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
         <span className='hash-span' id='experience'>
@@ -40,6 +49,14 @@ const Experience = () => {
             <h3 className='text-white font-medium text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>NIST SHIP</h3>
             <p className='mt-4 text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl'>2024: Identifying multi-objective optimization in an active learning loop for automated machine learning. Using Python with torch, tensorflow, jax, scipy, and scikit.</p>
           </div>
+        </motion.div>
+        <div className='text-center md:text-left md:px-20 lg:px-40'>
+          <motion.div variants={textVariant()}>
+            <h2 className='text-white font-bold md:text-[60px] sm:text-[37px] text-[30px]'>Research</h2>
+          </motion.div>
+        </div>
+        <motion.div ref={ref} animate={controls} initial="hidden" variants={fadeIn("up", "spring", 0, 0.75)} className='w-full mt-[-2px] flex'>
+          <ImageGallery items={images} />
         </motion.div>
     </motion.section>
   );
