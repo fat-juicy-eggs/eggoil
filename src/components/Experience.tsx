55b62eff258dbec2b2ba47a4ea5600ef4d27753c
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-02 10:52:42 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -14,7 +14,7 @@ const Experience = () => {
       }
   }, [controls, inView]);
   return (
-    <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0 h-[1vh]'>
+    <motion.section initial="hidden" whileInView='show' viewport={{ once:true, amount:0.25 }} className='px-0 2x1:px-60 py-10 2x1:py-16 max-w-full mx-auto relative z-0'>
         <span className='hash-span' id='experience'>
             &nbsp;
         </span>
@@ -23,11 +23,11 @@ const Experience = () => {
             <h2 className='text-white font-bold md:text-[60px] sm:text-[37px] text-[30px]'>Research: NIST SHIP</h2>
           </motion.div>
         </div>
-        <div className='w-full mt-[-2px] flex gap-5 mx-2 h-full'>
+        <div className='w-full mt-[-2px] flex gap-5 mx-2' style="height:1vh;>
           <motion.div ref={ref} animate={controls} initial='hidden' variants={fadeIn("up", "spring", 0, 0.75)}>
             <h4 className='text-white md:text-[30px] sm:text-[18px] text-[15px]'>This is the title of the paper</h4>
-            <div className='relative w-full mx-[5px] h-full'>
-              <iframe src="https://docs.google.com/gview?url=https://assets-global.website-files.com/603d0d2db8ec32ba7d44fffe/603d0e327eb2748c8ab1053f_loremipsum.pdf&embedded=true" className="w-full mx-[10px]"></iframe>
+            <div className='relative w-full mx-[5px]'>
+              <iframe src="https://docs.google.com/gview?url=https://assets-global.website-files.com/603d0d2db8ec32ba7d44fffe/603d0e327eb2748c8ab1053f_loremipsum.pdf&embedded=true" className="px-[10px]"></iframe>
             </div>
           </motion.div>
         </div>
