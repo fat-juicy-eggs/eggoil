4a1cd1eea6008edceadb4bfc219a23a7fa82363f
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-02 23:00:35 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Experience from "./Experience";
 import Hero from "./Hero";
-import Portfolio from "./Portfolio";
+import Projects from "./Portfolio";
 import ModelCanvas from "./of_planes_and_satellites";
 import Navbar from "./Navbar";
 import CanvasLoader from "./Loader";
