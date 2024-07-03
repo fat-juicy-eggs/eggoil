36e3375ab01dd7cba761650eb65808e250e82214
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-03 17:18:57 -0400

    Create Otherprojects.tsx

---
+++ src/components/Otherprojects.tsx
@@ -0,0 +1,43 @@
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import { useInView } from 'react-intersection-observer';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const Otherprojects = () => {
+    const controls = useAnimation();
+    const { ref, inView } = useInView({
+      threshold: 0,
+    });
+    useEffect(() => {
+      if (inView) {
+        controls.start("show");
+      }
+    }, [controls, inView]);
+    return (
+      <div style={{height: "20vh"}}></div>
+      <span className='hash-span' id='otherprojects'>
+          &nbsp;
+      </span>
+      <div className="section justify-items-end">
+          <div className='text-center md:text-left md:px-20 lg:px-40'>
+            <motion.div variants={textVariant()}>
+              <h2 className='text-white font-bold md:text-[60px] sm:text-[37px] text-[30px]'>Some of my other projects</h2>
+            </motion.div>
+          </div>
+          <motion.div ref={ref} animate={controls} initial="hidden" variants={fadeIn("up", "spring", 0, 0.75)} className='w-full mt-[-2px] flex items-center justify-center'>
+            <h3 className='text-white font-medium text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>CHILL AI: Modeling Heat Islands</h3>
+            <div className='mt-10 md:mt-20 flex flex-col gap-10 md:gap-20'>
+              <div className='relative w-full md:w-3/5'>
+                
+              </div>
+              <div className='w-full md:w-2/5 px-0 md:p-8 flex flex-col justify-center text-left md:text-left'>
+                <p className='mt-4 text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg 2xl:text-xl'>
+                  We created a computer vision model that classifies areas of urban heat islands and presented as a business pitch in the Moonshot Pirates Shape the Future challenge, earning 2nd place.
+                </p>
+              </div>
+          </motion.div>
+      </div>
+    );
+};
+
+export default Otherprojects;
