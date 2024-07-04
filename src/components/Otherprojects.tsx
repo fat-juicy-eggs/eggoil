723aad8abc49139eb1583054c1f6cf6a5a802d5d
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-04 11:41:53 -0400

    Update Otherprojects.tsx

--- src/components/Otherprojects.tsx
+++ src/components/Otherprojects.tsx
@@ -44,6 +44,7 @@ const Otherprojects = () => {
               </div>
               <div style={{height: "20vh"}}></div>
               <h3 className='text-white font-medium text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl lg:text-5xl leading-tight'>CHILL AI: Modeling Heat Islands</h3>
+              <div style={{height: "10vh"}}></div>
               <motion.div ref={ref} animate={controls} initial="hidden" variants={fadeIn("up", "spring", 0, 0.75)} className='w-full mt-[-2px] flex items-center justify-center'>
                 <div className='flex'>
                   <div className='relative w-full h-auto bg-secondary rounded-md'>
