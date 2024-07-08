26715c7a6905776615d3cc069e3a5060b14b4578
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-08 16:26:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import { useEffect, useRef } from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Experience, Hero, Navbar, Projects, Bio, Otherprojects } from "./components";
 
 const App = () => {
   const wrapperRef = useRef(null);
 
   return (
-    <BrowserRouter>
+    <Router>
       <div className='relative z-0 bg-primary'>
         <Navbar />
         <div className='wrapper' ref={wrapperRef}>
-          <div id="hero" className='z-10'>
-            <Hero scrollContainer={wrapperRef} />
-          </div>
-          <div id="bio" className='z-10'>
-            <Bio />
-          </div>
-          <div id='experience' className='z-10'>
-            <Experience />
-          </div>
-          <div id='models' className='z-10'>
-            <Projects />
-          </div>
-          <div id='projects' className='z-10'>
-            <Otherprojects />
-          </div>
+          <Switch>
+            <Route path="/" exact>
+              <div id="hero" className='z-10'>
+                <Hero scrollContainer={wrapperRef} />
+              </div>
+              <div id="bio" className='z-10'>
+                <Bio />
+              </div>
+              <div id='experience' className='z-10'>
+                <Experience />
+              </div>
+              <div id='models' className='z-10'>
+                <Projects />
+              </div>
+              <div id='projects' className='z-10'>
+                <Otherprojects />
+              </div>
+            </Route>
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
