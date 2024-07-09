7ea756a7791a9aa264ceb70aaf7bd4d822b9fd7a
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-09 12:21:25 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -90,7 +90,7 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-30 rounded-xl`}
+            } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-30 rounded-xl bg-black`}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
@@ -100,7 +100,7 @@ const Navbar = () => {
                     active === nav.id ? "text-quaternary" : "text-secondary"
                   }`}
                   onClick={() => {
-                    setToggle(!toggle);
+                    setToggle(false);
                     setActive(nav.id);
                   }}
                 >
