55b7a95226115a3e6a94161d8ece3175a688e1f6
fat-juicy-eggs <75511380+fat-juicy-eggs@users.noreply.github.com>
2024-07-09 16:53:33 -0400

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -38,7 +38,7 @@ const Experience = () => {
         className='w-full mt-[-2px] flex flex-col md:flex-row-reverse'
       >
         <div className='relative w-2/3 md:w-3/5'>
-          <Document file="https://assets-global.website-files.com/603d0d2db8ec32ba7d44fffe/603d0e327eb2748c8ab1053f_loremipsum.pdf" onLoadSuccess={onDocLoadSuccess}>
+          <Document file="https://pdfobject.com/pdf/sample.pdf" onLoadSuccess={onDocLoadSuccess}>
             <Page pageNumber={pageNumber} />
           </Document>
         </div>
